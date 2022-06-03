b6af0afb77599e597cd985c0a5fdbc680056dc40
mohddilshad76 <80386441+mohddilshad76@users.noreply.github.com>
2022-06-03 06:53:01 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -54,7 +54,7 @@ $('document').ready(function(){
 	function loopOne() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
-		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
+		$('#b1').animate({left:randleft,bottom:randtop},1000,function(){
 			loopOne();
 		});
 	}
